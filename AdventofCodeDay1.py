f5044fbcf711598d2e3676cb59611128f52f3960
katapples <32974032+katapples@users.noreply.github.com>
2020-12-01 11:18:50 -0500

    Update AdventofCodeDay1.py

--- AdventofCodeDay1.py
+++ AdventofCodeDay1.py
@@ -1,5 +1,12 @@
-#find the two numbers in a text file that add to 2020 and output their product when multiplied
-for i in range (arr len-1)
-    for j in range (arr len-2)
-        if arr[i]+arr[j] == 2020
-            print arr[i] + " " + arr[j]
+#find the two (or three for part two) numbers in a text file that add to 2020 and output their product 
+file = open(r"numbersForDay1.txt")
+arr = file.readlines()  #read all lines into an array
+
+for a in range(len(arr)):
+    arr[a] = int(arr[a]) #converting to integers
+# print arr #check the array
+
+for i in range (len(arr)-1): #go thru each number
+    for j in range (len(arr)): # and check it against every other number in the array
+        for k in range (len(arr)): # and check it against the third number for part two of the challenge
+            if  arr[i]+arr[j]+arr[k]== 2020: #output product if nunmbers sum to 2020
