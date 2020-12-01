a46653127e362b11eaec0b9f3c8537b09606f7ea
katapples <32974032+katapples@users.noreply.github.com>
2020-12-01 11:17:12 -0500

    Add files via upload

---
+++ AdventofCodeDay1.py
@@ -0,0 +1,5 @@
+#find the two numbers in a text file that add to 2020 and output their product when multiplied
+for i in range (arr len-1)
+    for j in range (arr len-2)
+        if arr[i]+arr[j] == 2020
+            print arr[i] + " " + arr[j]
